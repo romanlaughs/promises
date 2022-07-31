4e0adc867391be8291eff6ff2c78fca87a00358b
Nick Cassano <romanlaughs@gmail.com>
2022-07-31 10:29:35 -0700

    Complete bare_minimum/promisification.js exercises

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,27 @@ var fs = require('fs');
 var request = require('needle');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      return callback(err);
+    } else {
+      const result = data.split(/\r?\n/);
+      return callback(null, result[0]);
+    }
+  });
 };
 
+
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  request.get(url, 'utf8', (err, data) => {
+    if (err) {
+      return callback(err);
+    } else {
+      return callback(null, data.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -10,12 +10,29 @@ var Promise = require('bluebird');
 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+  return new Promise ((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        return reject(err);
+      } else {
+        const result = data.split(/\r?\n/);
+        return resolve(result[0]);
+      }
+    });
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
-  // TODO
+  return new Promise ((resolve, reject) => {
+    request.get(url, 'utf8', (err, data) => {
+      if (err) {
+        return reject(err);
+      } else {
+        return resolve(data.statusCode);
+      }
+    });
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
